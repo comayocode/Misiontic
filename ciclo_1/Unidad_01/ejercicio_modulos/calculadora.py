2b0a19a09950faa29963393be3cd4769b1ac64fd
roko <ronalc726@gmail.com>
2022-06-02 17:17:58 -0500

    Apunter terminados Unidad_01

---
+++ ciclo_1/Unidad_01/ejercicio_modulos/calculadora.py
@@ -0,0 +1,12 @@
+def suma(num_1, num_2):
+    return num_1 + num_2
+
+def resta (num_1, num_2):
+    return num_1 - num_2
+
+def multiplicacion (num_1, num_2):
+    return num_1 * num_2
+
+def division (num_1, num_2):
+    return num_1 / num_2
+
---
+++ ciclo_1/Unidad_01/ejercicio_modulos/main.py
@@ -0,0 +1,5 @@
+import calculadora
+
+suma = calculadora.suma(30, 5)
+print(suma)
+
