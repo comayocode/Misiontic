facc2ee3132cc664f92d53e1ae6d201f2831ecac
roko <ronalc726@gmail.com>
2022-06-10 18:32:41 -0500

    Solución error PATH

---
+++ ciclo_1/01_retos/reto_5/test_2.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+# ruta file csv
+rutaFileCsv = 'https://github.com/luisguillermomolero/MisionTIC2022_2/blob/master/Modulo1_Python_MisionTIC2022_Main/Semana_5/Reto/movies.csv?raw=true'
+
+#Dividir nombre y extensión del archivo
+root, extension = os.path.splitext("movies.csv")
+
+def listaPeliculas(rutaFileCsv: str) -> str:
+    #Validar extensión
+    if extension == ".csv":
+        try:
+            #Cargar archivo/ruta csv
+            moviesCsv = pd.read_csv(rutaFileCsv)
+            #Crear subconjunto/Subdataframe
+            subDataframe = moviesCsv.loc[:, ['Country','Language','Gross Earnings']]
+            #Creación pivot table
+            tablaDinámica = pd.pivot_table(subDataframe, index=['Country','Language'])
+
+            return tablaDinámica.head(10)
+        except:
+            return "Error al leer el archivo de datos."
+    else:
+        return "Extensión  inválida."
+
+print(listaPeliculas(rutaFileCsv))
