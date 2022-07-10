6818597e27116ac08e160a5277bcdbc5edb1ea3a
roko <ronalc726@gmail.com>
2022-07-10 17:39:31 -0500

    Modificación proyectos empresa git status a

--- ciclo_2/Unidad_02/Proyectos/empresa/src/main/java/com/empresa/App.java
+++ ciclo_2/Unidad_02/Proyectos/empresa/src/main/java/com/empresa/App.java
@@ -4,6 +4,14 @@ public class App
 {
     public static void main( String[] args ){
         //Crear relación entre empresa y app
-        Empresa objEmpresa = new Empresa("Tesla","126569","Calle 56 #12-54","empresa@empresa.com","056");
+        //Empresa objEmpresa = new Empresa("Tesla","126569","Calle 56 #12-54","empresa@empresa.com","056");
+
+        //CICLOS
+        //ciclos();
+    }
+    public static void ciclos(){
+        for(int i=0; i<5; i++ ){
+            System.out.println(i);
+        }
     }
 }
--- ciclo_2/Unidad_02/Proyectos/empresa/src/main/java/com/empresa/Empresa.java
+++ ciclo_2/Unidad_02/Proyectos/empresa/src/main/java/com/empresa/Empresa.java
@@ -1,5 +1,8 @@
 package com.empresa;
 
+import java.security.PublicKey;
+import java.sql.SQLOutput;
+
 public class Empresa {
     //ATRIBUTOS
     private String nombre;
@@ -9,7 +12,7 @@ public class Empresa {
     private String nit;
     private Carro[] carros; //Relación entre Carro y empresa
 
-    //CONSTRUTORES
+   //CONSTRUTORES
     public Empresa(String nombre, String telefono, String direccion, String email, String nit){
         this.nombre = nombre;
         this.telefono = telefono;
@@ -41,41 +44,72 @@ public class Empresa {
     public String getNombre() {
         return nombre;
     }
-
     public String getTelefono() {
         return telefono;
     }
-
     public String getDireccion() {
         return direccion;
     }
-
     public String getEmail() {
         return email;
     }
-
     public String getNit() {
         return nit;
+    }
+    //Consultores para arreglo
+    public Carro[] getCarros(){
+        return carros;
+    }
+    public Carro getCarro(int pos){
+        return carros[pos];
+    }
+    public int lengthCarro(){
+        return carros.length;
     }
 
     //MODIFICADORES
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
-
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public void setNit(String nit) {
         this.nit = nit;
     }
+    //Modificadores para arreglos
+    public void setCarros(Carro carro, int pos){
+        carros[pos] = carro;
+    }
+
+    public void clearCarros(){
+        //Recorrer el arreglo
+        int tamanio = carros.length;
+        for(int i = 0; i < carros.length; i++ ){
+            //Eliminar el dato
+            carros[i] = null;
+        }
+    }
+
+    //ACIONES
+    public void fabricarCarro(String placa, String modelo){
+        Carro carro = new Carro(placa, modelo);
+        boolean flag = false;
+        int pos = 0;
+        do {
+            if (carros[pos] == null){
+                carros[pos]=carro;
+                flag=true;
+            }
+            ++pos
+        }while (flag==false && pos<carros.length);
+    }
+
+
 }
