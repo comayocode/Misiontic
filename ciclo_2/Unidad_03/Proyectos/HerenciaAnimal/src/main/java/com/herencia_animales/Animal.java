5f04fc47cbd15ae4d412e96e574e64a0580c2446
roko <ronalc726@gmail.com>
2022-07-11 07:58:57 -0500

    Estructura básica proyecto herencia

---
+++ ciclo_2/Unidad_03/Proyectos/HerenciaAnimal/src/main/java/com/herencia_animales/Animal.java
@@ -0,0 +1,71 @@
+package com.herencia_animales;
+
+public class Animal {
+    private String color;
+    private double peso;
+    private double tamanio;
+    private boolean mamifero;
+    private boolean vertebrado;
+    private int edad;
+    private char genero;
+
+    public Animal(double peso, double tamanio, boolean mamifero, boolean vertebrado, int edad, char genero) {
+        this.peso = peso;
+        this.tamanio = tamanio;
+        this.mamifero = mamifero;
+        this.vertebrado = vertebrado;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+    // --- CONSULTORES ---
+    public String getColor() {
+        return color;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public double getTamanio() {
+        return tamanio;
+    }
+    public boolean isMamifero() {
+        return mamifero;
+    }
+    public boolean isVertebrado() {
+        return vertebrado;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public char getGenero() {
+        return genero;
+    }
+
+    // --- MODIFICADORES ---
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    // --- ACCIONES ---
+    public void comer(){
+        System.out.println("Comiendo...");
+    }
+    public void dormir(){
+        System.out.println("Durmiendo zzz...");
+    }
+    public void avanzar(){
+        System.out.println("Avanzando...");
+    }
+}
