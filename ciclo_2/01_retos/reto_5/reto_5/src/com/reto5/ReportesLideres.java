dbe72553409e219667342416b93d991f5e846645
roko <ronalc726@gmail.com>
2022-08-10 19:36:58 -0500

    Interfaz grafica UI - Menu y reportes lideres

---
+++ ciclo_2/01_retos/reto_5/reto_5/src/com/reto5/ReportesLideres.java
@@ -0,0 +1,289 @@
+
+package com.reto5;
+
+import java.awt.Color;
+
+public class ReportesLideres extends javax.swing.JFrame {
+
+    int xMouse, yMouse;
+    
+    public ReportesLideres() {
+        
+        initComponents();
+        
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtReportesLideres = new javax.swing.JTable();
+        jpBarraSuperior = new javax.swing.JPanel();
+        jpExit = new javax.swing.JPanel();
+        lbExit = new javax.swing.JLabel();
+        jpAtras = new javax.swing.JPanel();
+        lbAtras = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocationByPlatform(true);
+        setUndecorated(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jtReportesLideres.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(jtReportesLideres);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 801, 290));
+
+        jpBarraSuperior.setBackground(new java.awt.Color(255, 255, 255));
+        jpBarraSuperior.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jpBarraSuperiorMouseDragged(evt);
+            }
+        });
+        jpBarraSuperior.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jpBarraSuperiorMousePressed(evt);
+            }
+        });
+
+        jpExit.setBackground(new java.awt.Color(255, 255, 255));
+        jpExit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jpExit.setPreferredSize(new java.awt.Dimension(50, 50));
+        jpExit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jpExitMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jpExitMouseEntered(evt);
+            }
+        });
+
+        lbExit.setBackground(new java.awt.Color(0, 0, 0));
+        lbExit.setFont(new java.awt.Font("Roboto Light", 1, 28)); // NOI18N
+        lbExit.setForeground(new java.awt.Color(0, 0, 0));
+        lbExit.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lbExit.setText("X");
+        lbExit.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lbExit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lbExitMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lbExitMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lbExitMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jpExitLayout = new javax.swing.GroupLayout(jpExit);
+        jpExit.setLayout(jpExitLayout);
+        jpExitLayout.setHorizontalGroup(
+            jpExitLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpExitLayout.createSequentialGroup()
+                .addComponent(lbExit, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        jpExitLayout.setVerticalGroup(
+            jpExitLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpExitLayout.createSequentialGroup()
+                .addComponent(lbExit, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jpAtras.setBackground(new java.awt.Color(255, 255, 255));
+        jpAtras.setAlignmentX(100.0F);
+        jpAtras.setAlignmentY(100.0F);
+        jpAtras.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jpAtras.setPreferredSize(new java.awt.Dimension(50, 50));
+        jpAtras.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jpAtrasMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jpAtrasMouseEntered(evt);
+            }
+        });
+
+        lbAtras.setBackground(new java.awt.Color(0, 0, 0));
+        lbAtras.setFont(new java.awt.Font("Roboto Medium", 0, 28)); // NOI18N
+        lbAtras.setForeground(new java.awt.Color(0, 0, 0));
+        lbAtras.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lbAtras.setText("<");
+        lbAtras.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lbAtras.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lbAtrasMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lbAtrasMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lbAtrasMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jpAtrasLayout = new javax.swing.GroupLayout(jpAtras);
+        jpAtras.setLayout(jpAtrasLayout);
+        jpAtrasLayout.setHorizontalGroup(
+            jpAtrasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpAtrasLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(lbAtras, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        jpAtrasLayout.setVerticalGroup(
+            jpAtrasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpAtrasLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(lbAtras, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        javax.swing.GroupLayout jpBarraSuperiorLayout = new javax.swing.GroupLayout(jpBarraSuperior);
+        jpBarraSuperior.setLayout(jpBarraSuperiorLayout);
+        jpBarraSuperiorLayout.setHorizontalGroup(
+            jpBarraSuperiorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpBarraSuperiorLayout.createSequentialGroup()
+                .addComponent(jpAtras, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 720, Short.MAX_VALUE)
+                .addComponent(jpExit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        jpBarraSuperiorLayout.setVerticalGroup(
+            jpBarraSuperiorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpBarraSuperiorLayout.createSequentialGroup()
+                .addGroup(jpBarraSuperiorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jpExit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jpAtras, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        jPanel1.add(jpBarraSuperior, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, 50));
+
+        jLabel1.setBackground(new java.awt.Color(59, 19, 71));
+        jLabel1.setFont(new java.awt.Font("Roboto", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(59, 19, 71));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Informe de líderes");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 50, 400, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, 420));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void lbAtrasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbAtrasMouseClicked
+        menu menu = new menu();
+        menu.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_lbAtrasMouseClicked
+
+    private void lbAtrasMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbAtrasMouseEntered
+        jpAtras.setBackground(new Color(255, 175, 114));
+        lbAtras.setForeground(Color.BLACK);
+    }//GEN-LAST:event_lbAtrasMouseEntered
+
+    private void lbAtrasMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbAtrasMouseExited
+        jpAtras.setBackground(Color.WHITE);
+        lbAtras.setForeground(Color.BLACK);
+    }//GEN-LAST:event_lbAtrasMouseExited
+
+    private void jpExitMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpExitMouseClicked
+        
+    }//GEN-LAST:event_jpExitMouseClicked
+
+    private void jpExitMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpExitMouseEntered
+
+    }//GEN-LAST:event_jpExitMouseEntered
+
+    private void jpBarraSuperiorMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpBarraSuperiorMouseDragged
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        this.setLocation(x - xMouse, y - yMouse);
+    }//GEN-LAST:event_jpBarraSuperiorMouseDragged
+
+    private void jpBarraSuperiorMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpBarraSuperiorMousePressed
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }//GEN-LAST:event_jpBarraSuperiorMousePressed
+
+    private void lbExitMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbExitMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_lbExitMouseClicked
+
+    private void lbExitMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbExitMouseEntered
+        jpExit.setBackground(new Color(251, 51, 51));
+        lbExit.setForeground(Color.WHITE);
+    }//GEN-LAST:event_lbExitMouseEntered
+
+    private void lbExitMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbExitMouseExited
+        jpExit.setBackground(Color.WHITE);
+        lbExit.setForeground(Color.BLACK);
+    }//GEN-LAST:event_lbExitMouseExited
+
+    private void jpAtrasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpAtrasMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jpAtrasMouseClicked
+
+    private void jpAtrasMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpAtrasMouseEntered
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jpAtrasMouseEntered
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ReportesLideres.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ReportesLideres.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ReportesLideres.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ReportesLideres.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ReportesLideres().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JPanel jpAtras;
+    private javax.swing.JPanel jpBarraSuperior;
+    private javax.swing.JPanel jpExit;
+    public javax.swing.JTable jtReportesLideres;
+    private javax.swing.JLabel lbAtras;
+    private javax.swing.JLabel lbExit;
+    // End of variables declaration//GEN-END:variables
+}
