57e2699945a42c265e14935faae48b13db92f795
roko <ronalc726@gmail.com>
2022-08-14 16:32:15 -0500

    Archivos organizados

--- ciclo_2/01_retos/reto_5/DB/Consultas.sql
+++ ciclo_2/01_retos/reto_5/DB/Consultas.sql
@@ -2,7 +2,7 @@
 SELECT ID_Lider, Nombre, Primer_Apellido, Ciudad_Residencia  FROM Lider ORDER BY Ciudad_Residencia ASC;
 
 --SEGUNDA CONSULTA
-SELECT ID_Proyecto, Clasificacion, Constructora, Numero_Habitaciones, Ciudad FROM Proyecto WHERE Ciudad IN ('Cartagena', 'Barranquilla', 'Santa Marta') AND Clasificacion = 'Casa Campestre';
+SELECT ID_Proyecto, Clasificacion, Constructora, Numero_Habitaciones , Ciudad FROM Proyecto WHERE Ciudad IN ('Cartagena', 'Barranquilla', 'Santa Marta') AND Clasificacion = 'Casa Campestre';
 
 --TERCERA CONSULTA
 SELECT c.ID_Compra, c.Proveedor, p.Ciudad, p.Constructora, p.Banco_Vinculado FROM Compra c INNER JOIN Proyecto p ON c.ID_Proyecto = p.ID_Proyecto WHERE c.Proveedor = 'Homecenter' AND p.Ciudad = 'Salento';
